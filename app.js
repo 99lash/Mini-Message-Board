b2f2277aa71c1af4fbf81e0adf5eed5e852b2800
Asher <asherjohn48@gmail.com>
2025-06-10 18:07:12 +0800

    fix static path

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 3001;
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({extended: true}));
 
 app.use('/messages', messagesRouter);
